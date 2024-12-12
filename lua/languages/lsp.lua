a8fc8cb0d2f7e31b5afb33452aa8e37b0ec7e557
Serhii Shvets <wert2all@gmail.com>
2024-12-12 21:35:59 +0200

    feat: add all languages for lsp

--- lua/languages/lsp.lua
+++ lua/languages/lsp.lua
@@ -1,1 +1,22 @@
-return { "angularls" }
+return {
+    "angularls",
+    "ansiblels",
+    "bashls",
+    "docker_compose_language_service",
+    "dockerls",
+    "gopls",
+    "html",
+    "cssls",
+    "emmet_ls",
+    "hyprls",
+    "jsonls",
+    "marksman",
+    "nixd",
+    "phpactor",
+    "sqls",
+    "tailwindcss",
+    "vtsls",
+    "eslint",
+    "lemminx",
+    "yamlls",
+}
